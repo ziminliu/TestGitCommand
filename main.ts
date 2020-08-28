ddd4e2b407ed652267584903f446d782067d13d9
zimin <1772796436@qq.com>
2020-08-28 10:59:22 +0800

    git commit --amend --no-edit

--- main.ts
+++ main.ts
@@ -3,3 +3,7 @@ console.log('hello typescript')
 console.log('merge again ,delete No.2 row')
 
 console.log('add a new line')
+
+let str : string = 'git commit --amend --no-edit'
+
+console.log('hello ' + str)
